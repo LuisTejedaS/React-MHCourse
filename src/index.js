42fd99554193d2fd4b28b8cfacee58ae912e42c1
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-01-03 14:46:37 -0600

    counters component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 //import './index.css';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./components/counters";
 
- 
-ReactDOM.render(<Counter></Counter>, document.getElementById("root"));
+ReactDOM.render(<Counters></Counters>, document.getElementById("root"));
